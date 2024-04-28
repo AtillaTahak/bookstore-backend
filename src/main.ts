517937b775d761112633de603b128c198efe71ae
AtillaTahak <atillatahaa@gmail.com>
2024-04-28 16:12:34 +0300

    Feature(auth,user): Create auth, Add swagger

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { useContainer } from 'class-validator';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Bookstore')
+    .setDescription('The bookstore API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('bookstore')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
